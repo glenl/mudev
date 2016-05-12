3c1619f3f4bd07efa1a40035d42bd7c9f2addf98
Glen Larsen <glenl.glx@gmail.com>
2016-05-11 23:30:43 -0700

    set allowed_hosts to let heroku work

--- mudev/settings.py
+++ mudev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*joliphfw&jhh(eyf#y#)=1_%=%@g(&^i&*&dsfd=^w7if5_ol'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Application definition
 
