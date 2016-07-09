fbdf3a7ab3515560dad6f071d55af07465947c96
Glen Larsen <glenl.glx@gmail.com>
2016-07-09 12:36:59 -0700

    Add capitalization check

--- mutopia/tests/test_db.py
+++ mutopia/tests/test_db.py
@@ -12,11 +12,17 @@ class RawInstrumentTestCase(TestCase):
         RawInstrumentMap.objects.create(raw_instrument='uke', instrument=cls.ukulele)
 
     def test_can_translate(self):
+        # test a match via the mapping
         g = instrument_match('axe')
         self.assertEqual(g, self.guitar)
+        # an instrument should always match itself
         g = instrument_match('guitar')
         self.assertEqual(g, self.guitar)
         uke = instrument_match('uke')
         self.assertEqual(uke, self.ukulele)
+        # abbreviations don't work here
         uke = instrument_match('uk')
         self.assertEqual(uke, None)
+        # but capitalization is ignored
+        uke = instrument_match('Uke')
+        self.assertEqual(uke, self.ukulele)
